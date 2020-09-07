f7dd2cc0b35d85c3aacab958288feb2b00eec6fa
миша миша <mhailovcinnikov2@gmail.com>
2020-09-07 12:20:35 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+
+            InitializeComponent();
+            label3.Visible = false;
+            StreamWriter File = new StreamWriter("File.txt");
+            File.WriteLine("admin");
+            File.WriteLine("Admin@123");
+
+            File.Close();
+
+        }
+
+       public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+            
+           
+           StringBuilder Login = new StringBuilder(textBox2.Text);
+           StringBuilder pasword = new StringBuilder(textBox1.Text);
+            String[] str = System.IO.File.ReadAllLines(Login+".txt");
+
+            string s = textBox1.Text;
+
+            if ((s.Length >= 6 && s.Any(char.IsUpper) && s.Any(char.IsDigit) &&
+                s.IndexOfAny(new char[] { '!', '@', '#', '$', '%', '^' }) >= 0) &&
+                (str[0] == Login.ToString() && str[1] == pasword.ToString()))
+            {
+                MessageBox.Show("Вход прошел успешно");
+                Application.Exit();
+
+
+            }
+
+            else   { MessageBox.Show("Данные введены неверно"); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Form3 frm1 = new Form3();
+            frm1.Show();
+            this.Visible = false;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form3.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            {
+                string s = textBox2.Text;
+                if ((s.Length >= 6 && s.Any(char.IsUpper) && s.Any(char.IsDigit) &&
+                   s.IndexOfAny(new char[] { '!', '@', '#', '$', '%', '^' }) >= 0))
+                {
+                    string log = Convert.ToString(textBox1.Text);
+                    StreamWriter qw = new StreamWriter(log + ".txt");
+                    qw.WriteLine(textBox1.Text);
+                    qw.WriteLine(textBox2.Text);
+                    qw.Close();
+                    {
+                        Form1 d2 = new Form1();
+                        d2.Show();
+                        this.Close();
+                    }
+                }
+                else if (s.Length < 6)
+                {
+                    MessageBox.Show("В пароле необходимо минимум 6 символов");
+                }
+                else if (s.Any(char.IsUpper) == false)
+                {
+                    MessageBox.Show("В пароле нужна минимум 1 прописная буква");
+                }
+                else if (s.Any(char.IsDigit) == false)
+                {
+                    MessageBox.Show("В пароле нужна минимум 1 цифра");
+                }
+                else if (s.IndexOfAny(new char[] { '!', '@', '#', '$', '%', '^' }) < 0)
+                {
+                    MessageBox.Show("В пароле нужен минимум один символ из набора: ! @ # $ % ^");
+                }
+                
+            }
+
+
+
+
+
+
+            
+        
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-Минимум 6 символов. \n-Минимум 1 прописная буква." +
+                " \n-Минимум 1 цифра. \n-Минимум один символ из набора: ! @ # $ % ^ ", "Условия пароля");
+
+        }
+    }
+}
